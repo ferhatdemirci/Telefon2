a3cfa7630d23e64205ea494abac177103aa905ee
ferhat.demirci <ferhat.demirci@BDT-KAY-BT-02>
2017-03-13 11:53:47 +0300

    2. Yeni class commit

---
+++ Telefon2/Yeni2Class.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telefon2
+{
+    class Yeni2Class
+    {
+        //2.YeniClass
+    }
+}
